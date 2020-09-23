cde58026cdf11e556ac4013fefbf7d5d8a5d75a2
BILAL KHAN <khanbilalmohammad1510@gmail.com>
2020-09-23 12:34:21 +0530

    Function for computing sum of the list is added

---
+++ Assignment2/tutorial02.py
@@ -0,0 +1,88 @@
+# # All decimal 3 places
+# a=3.4
+# a="{0:.3f}".format(a)
+# print(a)
+# Function to compute mean
+import math
+
+def sum_of_list(first_list):
+    a=0
+    for i in range(len(first_list)):
+        a+=first_list[i]
+    return a
+
+# def mean(first_list):
+#     if len(first_list) is 0:
+#         return 0
+#     mean_value= sum_of_list(first_list)/len(first_list)
+#     mean_value="{0:.3f}".format(mean_value)
+#     return mean_value
+# print(mean([1,2,3,4]))
+# # Function to compute median. You cant use Python functions
+# def median(first_list):
+#     # median Logic
+#     return median_value
+
+
+# # Function to compute Standard deviation. You cant use Python functions
+# def standard_deviation(first_list):
+#     # Standard deviation Logic
+#     return standard_deviation_value
+
+
+# # Function to compute variance. You cant use Python functions
+# def variance(first_list):
+#     # variance Logic
+#     return variance_value
+
+
+# # Function to compute RMSE. You cant use Python functions
+# def rmse(first_list, second_list):
+#     # RMSE Logic
+#     return rmse_value
+
+
+# # Function to compute mse. You cant use Python functions
+# def mse(first_list, second_list):
+#     # mse Logic
+#     return mse_value
+
+
+# # Function to compute mae. You cant use Python functions
+# def mae(first_list, second_list):
+#     # mae Logic
+#     return mae_value
+
+
+# # Function to compute NSE. You cant use Python functions
+# def nse(first_list, second_list):
+#     # nse Logic
+#     return nse_value
+
+
+# # Function to compute Pearson correlation coefficient. You cant use Python functions
+# def pcc(first_list, second_list):
+#     # nse Logic
+#     return pcc_value
+
+
+# # Function to compute Skewness. You cant use Python functions
+# def skewness(first_list):
+#     # Skewness Logic
+#     return skewness_value
+    
+# def sorting(first_list):
+#     # Sorting Logic
+#     return sorted_list
+
+
+# # Function to compute Kurtosis. You cant use Python functions
+# def kurtosis(first_list):
+#     # Kurtosis Logic
+#     return kurtosis_value
+
+
+# # Function to compute sum. You cant use Python functions
+# def summation(first_list):
+#     # sum Logic
+#     return summation_value
