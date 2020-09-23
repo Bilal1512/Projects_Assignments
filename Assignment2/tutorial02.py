673530299fe92e11a15bb4dafe8922a1c0c182a6
BILAL KHAN <khanbilalmohammad1510@gmail.com>
2020-09-23 14:42:05 +0530

    Function for calculating Median of the list is added

--- Assignment2/tutorial02.py
+++ Assignment2/tutorial02.py
@@ -17,12 +17,27 @@ def mean(first_list):
     mean_value = sum_of_list(first_list)/len(first_list)
     return round(mean_value,3)
 
-# # Function to compute median. You cant use Python functions
-# def median(first_list):
-#     # median Logic
-#     return median_value
+def sort_list(first_list):
+    for i in range(len(first_list)):
+        for j in range(i,len(first_list)):
+            if(first_list[j]<first_list[i]):
+                first_list[i],first_list[j]=first_list[j],first_list[i]
+    return first_list
+
+# Function to compute median. You cant use Python functions
+def median(first_list):
+    if len(first_list) is 0:
+        return 0
+    first_list=sort_list(first_list)
+    if len(first_list)%2 is 1:
+        median_value=first_list[int(len(first_list)/2)]
+    else:
+        median_value=(first_list[int(len(first_list)/2)-1]+first_list[int(len(first_list)/2)])/2
+    return round(median_value,3)
 
 
+print(median([3,1,4,2,5,1]))
+print(sort_list([3,1,4,2,5,1]))
 # # Function to compute Standard deviation. You cant use Python functions
 # def standard_deviation(first_list):
 #     # Standard deviation Logic
