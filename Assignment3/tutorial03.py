5226af2c122658cf91851721b2f77ddb1a221562
BILAL KHAN <khanbilalmohammad1510@gmail.com>
2020-10-15 19:43:28 +0530

    Assignment 3: Function for classification of students based on course is added

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -18,3 +18,39 @@ def del_create_analytics_folder():
     path=os.getcwd()+'\\'+'analytics'
     if os.path.exists(path):
         shutil.rmtree(path)
+
+def course():
+    with open('./studentinfo_cs384.csv','r') as file:
+        reader=csv.reader(file)
+        for i in reader:
+            roll_number=i[0];
+            roll_number=roll_number.lower()
+            if valid_roll(roll_number):
+                year=roll_number[0:2]
+                branch_code=roll_number[4:6]
+                degree=roll_number[2:4]
+                if degree=='01':
+                    degree='btec'
+                elif degree=='11':
+                    degree='mtec'
+                elif degree=='12':
+                    degree='msc'
+                elif degree=='21':
+                    degree='phd'
+                path=os.getcwd()+'\\'+'analytics'+'\\'+'course'+'\\'+branch_code+'\\'+degree
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                if not os.path.exists(path+'\\'+year+'_'+branch_code+'_'+degree+'.csv'):
+                    with open(path+'\\'+year+'_'+branch_code+'_'+degree+'.csv', 'a',newline='') as fill:
+                        writer=csv.writer(fill)
+                        writer.writerow(['id','full_name','country','email','gender','dob','blood_group','state'])
+                with open(path+'\\'+year+'_'+branch_code+'_'+degree+'.csv', 'a',newline='') as fill:
+                    writer=csv.writer(fill)
+                    writer.writerow(i)
+            else:
+                if not os.path.exists(os.getcwd()+'\\'+'analytics'+'\\'+'course'):
+                    os.makedirs(os.getcwd()+'\\'+'analytics'+'\\'+'course')
+                with open(os.getcwd() + '\\' +'analytics'+'\\'+'course'+'\\'+'misc'+'.csv', 'a', newline='') as file:
+                    writer = csv.writer(file)
+                    writer.writerow(i)
+    pass
